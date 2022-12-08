62ef1912faa7257dbd6eba9a25af6982f65237a9
Braulio Santana <bs1386@g.rit.edu>
2022-12-08 00:19:03 -0500

    some day7

--- Day07.cpp
+++ Day07.cpp
@@ -27,11 +27,43 @@ struct node
 {
 	string name;
 	string type; // dir, file
-	struct node *list;
+	std::vector<struct node> list;
 	int size;
 };
 
 void Day07::Parse()
+{
+
+	string name{};
+	string cmd{};
+	std::vector<struct node> directory{};
+
+	for (int row = 0; row < m_FileContents.size(); row++)
+	{
+		for (int col = 0; col < m_FileContents[row].size(); col++)
+		{
+			if (col == 0 && m_FileContents[row][col] == '$')
+			{
+				cmd = m_FileContents[row].substr(2, 2); 
+
+				if (cmd == "cd")
+				{
+					name = m_FileContents[row].substr(5, m_FileContents[row].size()-5);
+					node n;
+					n.name = name;
+					n.type = "dir";
+					n.size = 0;
+				}
+				else if (cmd == "ls")
+				{
+
+				}
+			}
+		}
+	}
+}
+
+void Day07::PerformCmd(string cmd) 
 {
 
 }

e563ddcb40f10a1cf69b5515e66456c494d506ea
bsantana-23 <brauliosantana17@gmail.com>
2022-12-08 11:29:07 -0500

    Work update

--- Day07.cpp
+++ Day07.cpp
@@ -27,19 +27,26 @@ struct node
 {
 	string name;
 	string type; // dir, file
+	string parent;
 	std::vector<struct node> list;
 	int size;
 };
 
 void Day07::Parse()
 {
-
+	string home{ '/' };
 	string name{};
 	string cmd{};
 	std::vector<struct node> directory{};
+	node curr_node{};
+	node head{};
 
 	for (int row = 0; row < m_FileContents.size(); row++)
 	{
+		if (row == m_FileContents.size() - 1)
+		{
+			cout << "l" << endl;
+		}
 		for (int col = 0; col < m_FileContents[row].size(); col++)
 		{
 			if (col == 0 && m_FileContents[row][col] == '$')
@@ -49,21 +56,91 @@ void Day07::Parse()
 				if (cmd == "cd")
 				{
 					name = m_FileContents[row].substr(5, m_FileContents[row].size()-5);
-					node n;
-					n.name = name;
-					n.type = "dir";
-					n.size = 0;
+					
+					if (curr_node.name == "")
+					{
+						node n;
+						n.name = name;
+						n.type = "dir";
+						n.size = 0;
+						curr_node = n;
+						head = n;
+					}
+
+					if (name == home)
+					{
+						break;
+					}
+					else if (name == "..")
+					{
+						for (int i = 0; i < curr_node.list.size(); i++)
+						{
+							if (curr_node.parent == curr_node.list[i].name)
+							{
+								curr_node = curr_node.list[i];
+								break;
+							}
+						}
+						break;
+					}
+					else
+					{
+						for (int i = 0; i < curr_node.list.size(); i++)
+						{
+							if (name == curr_node.list[i].name)
+							{
+								curr_node = curr_node.list[i];
+								break;
+							}
+							else 
+							{
+								cout << "node not in list" << endl;
+							}
+						}
+						break;
+					}
 				}
 				else if (cmd == "ls")
 				{
+					int i = row+1;
+					for (; i < m_FileContents.size() - 1 && m_FileContents[i][0] != '$'; i++)
+					{
+						// if it ls a dir
+						if (m_FileContents[i][0] == 'd')
+						{
+							name = m_FileContents[i].substr(4, m_FileContents[i].size() - 4);
+							
+							node n;
+							n.name = name;
+							n.type = "dir";
+							n.size = 0;
+							n.parent = curr_node.name;
 
+							curr_node.list.push_back(n);
+						}
+
+						// if it ls a file
+						else
+						{
+							int idx = 0;
+							for (; m_FileContents[i][idx] != ' '; idx++)
+							{
+
+							}
+							node n;
+							n.type = "file";
+							n.size = stoi(m_FileContents[i].substr(0, idx));
+							n.name = m_FileContents[i].substr(idx, m_FileContents[i].size() - idx);
+							n.parent = curr_node.name;
+
+							curr_node.list.push_back(n);
+						}
+					}
+					row = i-1;
+					break;
 				}
 			}
 		}
+		cout << m_FileContents[row] << endl;
 	}
-}
-
-void Day07::PerformCmd(string cmd) 
-{
-
 }

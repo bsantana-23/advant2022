1147700efaecce6e946e595360c8aec27f724e23
Braulio Santana <bs1386@g.rit.edu>
2022-12-09 01:42:16 -0500

    update from home

--- Day08.cpp
+++ Day08.cpp
@@ -30,79 +30,158 @@ void Day08::Parse()
 	m_Part1 += height * 2;
 	m_Part1 += (width - 2) * 2;
 
-	for (int row = 1; row < m_FileContents.size() - 1; row++)
+	//for (int row = 1; row < m_FileContents.size() - 2; row++)
+	//{
+	//	for (int col = 1; col < m_FileContents[row].size() - 1; col++)
+	//	{
+	//		int curr = int(m_FileContents[row][col]) - 48;
+	//		bool showing = true;
+
+	//		// Part 1
+	//		// Search above
+	//		for (int above = row-1; above >= 0; above--)
+	//		{
+	//			//int val = int(m_FileContents[above][col]) - 48;
+	//			if (int(m_FileContents[above][col]) - 48 >= curr)
+	//			{
+	//				showing = false;
+	//				break;
+	//			}
+	//		}
+
+	//		if (showing)
+	//		{
+	//			m_Part1++;
+	//			continue;
+	//		}
+	//		showing = true;
+
+	//		// Search below
+	//		for (int below = row+1; below <= m_FileContents.size()-1; below++)
+	//		{
+	//			if (int(m_FileContents[below][col]) - 48 >= curr)
+	//			{
+	//				showing = false;
+	//				break;
+	//			}
+	//		}
+
+	//		if (showing)
+	//		{
+	//			m_Part1++;
+	//			continue;
+	//		}
+	//		showing = true;
+
+	//		// Search left
+	//		for (int left = col-1; left >= 0; left--)
+	//		{
+	//			if (int(m_FileContents[row][left]) - 48 >= curr)
+	//			{
+	//				showing = false;
+	//				break;
+	//			}
+	//		}
+
+	//		if (showing)
+	//		{
+	//			m_Part1++;
+	//			continue;
+	//		}
+	//		showing = true;
+	//		
+	//		// Search right
+	//		for (int right = col + 1; right <= m_FileContents.size() - 1; right++)
+	//		{
+	//			if (int(m_FileContents[row][right]) - 48 >= curr)
+	//			{
+	//				showing = false;
+	//				break;
+	//			}
+	//		}
+
+	//		if (showing)
+	//		{
+	//			m_Part1++;
+	//			continue;
+	//		}
+	//	}
+	//}
+
+	// Part 2
+	for (int row = 1; row < m_FileContents.size() - 2; row++)
 	{
 		for (int col = 1; col < m_FileContents[row].size() - 1; col++)
 		{
 			int curr = int(m_FileContents[row][col]) - 48;
-			bool showing = true;
+			int u{ 0 };
+			int d{ 0 };
+			int l{ 0 };
+			int r{ 0 };
+			int highest_score{ 0 };
+			int temp{ 0 };
 
 			// Search above
-			for (int above = row-1; above >= 0; above--)
+			for (int above = row - 1; above >= 0; above--)
 			{
 				//int val = int(m_FileContents[above][col]) - 48;
 				if (int(m_FileContents[above][col]) - 48 >= curr)
 				{
-					showing = false;
+					u++;
 					break;
+				}
+				else
+				{
+					u++;
 				}
 			}
 
-			if (showing)
-			{
-				m_Part1++;
-				continue;
-			}
-			showing = true;
-
 			// Search below
-			for (int below = row+1; below <= m_FileContents.size()-1; below++)
+			for (int below = row + 1; below <= m_FileContents.size() - 1; below++)
 			{
 				if (int(m_FileContents[below][col]) - 48 >= curr)
 				{
-					showing = false;
+					d++;
 					break;
+				}
+				else
+				{
+					d++;
 				}
 			}
 
-			if (showing)
-			{
-				m_Part1++;
-				continue;
-			}
-			showing = true;
-
 			// Search left
-			for (int left = col-1; left >= 0; left--)
+			for (int left = col - 1; left >= 0; left--)
 			{
 				if (int(m_FileContents[row][left]) - 48 >= curr)
 				{
-					showing = false;
+					l++;
 					break;
+				}
+				else
+				{
+					l++;
 				}
 			}
 
-			if (showing)
-			{
-				m_Part1++;
-				continue;
-			}
-			showing = true;
-			
 			// Search right
 			for (int right = col + 1; right <= m_FileContents.size() - 1; right++)
 			{
 				if (int(m_FileContents[row][right]) - 48 >= curr)
 				{
-					showing = false;
+					r++;
 					break;
+				}
+				else
+				{
+					r++;
 				}
 			}
 
-			if (showing)
+			int scenic_score = l * r * u * d;
+			if (scenic_score > m_Part2)
 			{
-				m_Part1++;
-				continue;
+				m_Part2 = scenic_score;
 			}
 		}
 	}
